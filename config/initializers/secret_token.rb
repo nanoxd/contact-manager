340575c8f9f062e358ce7ebfa96c85de7c3f4145
Fernando Paredes <nano@fdp.io>
2013-08-22 19:05:42 -0700

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactManager::Application.config.secret_token = '02568425b4d3ec8535a4d487837f540e35cefebcde186bb686e738d4ad5ef1065c8210acfe24c9c3dddc3cb83ca3eeeca354c1c6ef0213cb82b226d4f359626d'

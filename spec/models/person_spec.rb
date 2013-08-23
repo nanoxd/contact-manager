3a3297fce401a6734e59ef2b4c15bdd410be6e9c
Fernando Paredes <nano@fdp.io>
2013-08-22 20:00:55 -0700

    Add test for validations in Person model

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:person) do
+    Person.new(first_name: 'Peter', last_name: 'Parker')
+  end
+
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    person.first_name = nil
+    expect(person).not_to be_valid
+  end
+
+  it 'is invalid without a last name' do
+    person.last_name = nil
+    expect(person).not_to be_valid
+  end
 end

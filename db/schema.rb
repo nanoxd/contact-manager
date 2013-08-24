12d694a3918c89e316f509d9b91e8acbd1fac258
Fernando Paredes <nano@fdp.io>
2013-08-24 11:16:14 -0700

    Add generated EmailAddress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130823032235) do
+ActiveRecord::Schema.define(:version => 20130824181353) do
+
+  create_table "email_addresses", :force => true do |t|
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "people", :force => true do |t|
     t.string   "first_name"

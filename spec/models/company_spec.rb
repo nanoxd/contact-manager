9ae260f8f87683286f8afc00793a1d5a7870289e
Fernando Paredes <nano@fdp.io>
2013-08-25 15:00:59 -0700

    Add validation for Company name

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Company do
+  let(:company) { Company.new(name: 'Compania') }
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
+
 end

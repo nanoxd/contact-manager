6de27a88a257ea3510611415977a703b9db66d1a
Fernando Paredes <nano@fdp.io>
2013-08-27 09:23:57 -0700

    Refactor PhoneNumbers controller

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,4 +1,5 @@
 class PhoneNumbersController < ApplicationController
+  before_filter :lookup_phone_number, only: [:edit, :update, :destroy]
   # GET /phone_numbers/new
   # GET /phone_numbers/new.json
   def new
@@ -12,7 +13,6 @@ class PhoneNumbersController < ApplicationController
 
   # GET /phone_numbers/1/edit
   def edit
-    @phone_number = PhoneNumber.find(params[:id])
   end
 
   # POST /phone_numbers
@@ -34,8 +34,6 @@ class PhoneNumbersController < ApplicationController
   # PUT /phone_numbers/1
   # PUT /phone_numbers/1.json
   def update
-    @phone_number = PhoneNumber.find(params[:id])
-
     respond_to do |format|
       if @phone_number.update_attributes(params[:phone_number])
         format.html { redirect_to @phone_number.contact, notice: 'Phone number was successfully updated.' }
@@ -50,7 +48,6 @@ class PhoneNumbersController < ApplicationController
   # DELETE /phone_numbers/1
   # DELETE /phone_numbers/1.json
   def destroy
-    @phone_number = PhoneNumber.find(params[:id])
     @phone_number.destroy
 
     respond_to do |format|
@@ -58,4 +55,10 @@ class PhoneNumbersController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  private
+
+    def lookup_phone_number
+      @phone_number = PhoneNumber.find(params[:id])
+    end
 end

5e8f939bd8d4fe6a56ba460512cfe1f4151e1fab
Fernando Paredes <nano@fdp.io>
2013-08-27 09:49:16 -0700

    Add a concern for Contact

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,5 @@
 class Company < ActiveRecord::Base
   attr_accessible :name
   validates :name, presence: true
-  has_many :phone_numbers, as: :contact
-  has_many :email_addresses, as: :contact
+  include Contact
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,6 @@
 class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
-
   validates :first_name, :last_name, presence: true
-  has_many :phone_numbers, as: :contact
-  has_many :email_addresses, as: :contact
+
+  include Contact
 end

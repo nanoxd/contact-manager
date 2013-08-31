4b62ffa14b93386dc4c1cdc78c0d884a86074792
Fernando Paredes <nano@fdp.io>
2013-08-30 17:32:28 -0700

    Configure Fabricator for Companies and add relationship to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130830055044) do
+ActiveRecord::Schema.define(:version => 20130831002631) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "email_addresses", :force => true do |t|

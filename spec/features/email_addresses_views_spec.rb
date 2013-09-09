edebd6c009fd1a284c16f51f301f08053b135fd1
Fernando Paredes <nano@fdp.io>
2013-09-09 09:24:50 -0700

    Fix button placement

--- spec/features/email_addresses_views_spec.rb
+++ spec/features/email_addresses_views_spec.rb
@@ -11,13 +11,13 @@ describe EmailAddress, type: :feature do
     it "shows the person's name in the title" do
       visit person_path(person)
       page.click_link('Add email address')
-      expect(page).to have_selector("h1", text: "#{person.last_name}, #{person.first_name}")
+      expect(page).to have_selector("h5", text: "#{person.last_name}, #{person.first_name}")
     end
 
     it "shows a company's name in the title" do
       visit company_path(company)
       page.click_link('Email Address')
-      expect(page).to have_selector("h1", text: "#{company.name}")
+      expect(page).to have_selector("h5", text: "#{company.name}")
     end
 
   end
@@ -34,7 +34,7 @@ describe EmailAddress, type: :feature do
 
       visit person_path(person)
       first(:link, 'edit').click
-      expect(page).to have_selector("h1", text: "#{person.last_name}, #{person.first_name}")
+      expect(page).to have_selector("h5", text: "#{person.last_name}, #{person.first_name}")
     end
 
     it "shows a company's name in the title" do
@@ -43,7 +43,7 @@ describe EmailAddress, type: :feature do
 
       visit company_path(company)
       first(:link, 'edit').click
-      expect(page).to have_selector("h1", text: "#{company.name}")
+      expect(page).to have_selector("h5", text: "#{company.name}")
     end
 
   end
